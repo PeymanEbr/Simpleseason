e5aa8124f17998173be63ab73620ce4b3228f1ab
DESKTOP-G68T79M\peyman <peyman@gmail.com>
2023-02-11 13:19:02 +0330

    update 1

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,16 +1,31 @@
 import React from "react";
 
+const seasonConfig = {
+    summer: {
+        text: "let's hit the beach",
+        iconName: "sun",
+    },
+    winter: {
+        text: "Burr it's cold",
+        iconName: "snowflake",
+    }
+}
 const findSeason = (lat,month) => {
     if (month > 2 && month < 9) {
-        return lat > 0 ? 'Summer' : 'winter';
+        return lat > 0 ? 'summer' : 'winter';
     }else {
-        return lat > 0 ? 'Winter' : 'Summer';
+        return lat > 0 ? 'winter' : 'summer';
     }
 };
 
 const SeasonDisplay = (props) => {
     const season = findSeason(props.lat,new Date().getMonth());
-    return <div>Season: {season}</div>
+    const {text,iconName} = seasonConfig[season];
+    return <div>
+        <i className={`${iconName} icon`}/>
+        <h1>{text}</h1>
+        <i className={`${iconName} icon`} />
+        </div>
 };
 
 export default SeasonDisplay;
